f2c20808542f254ec72bd54cbfb598e8cd92bf61
Antonio Marcos Sousa de Oliveira <antonioso@baraodemaua.br>
2026-02-20 09:24:16 -0300

    ajuste de anotação from body

--- Teste/obj/Debug/net8.0/Teste.AssemblyInfo.cs
+++ Teste/obj/Debug/net8.0/Teste.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07624eaa0a82a5ae7bd566835fc92b48a9739809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e95a2901ea2288015002e1869cc6a54c705289")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
